c6359373f89be07a384a6d0ecbddb2a824de587a
hackiodtech <geddyprime@gmail.com>
2023-12-19 03:30:09 -0800

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """A class that creates a square object"""
+
+    def __init__(self, size):
+        """Initialize a class Square.
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
